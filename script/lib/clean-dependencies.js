44c4cf235c7802db398149a4295abaaa9503d17d
Benjamin Gray <bengray178@gmail.com>
2020-09-13 23:07:36 +1000

    make clean-dependencies async

--- script/lib/clean-dependencies.js
+++ script/lib/clean-dependencies.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const CONFIG = require('../config');
 
-module.exports = function() {
+module.exports = async function() {
   // We can't require fs-extra or glob if `script/bootstrap` has never been run,
   // because they are third party modules. This is okay because cleaning
   // dependencies only makes sense if dependencies have been installed at least
@@ -10,33 +10,41 @@ module.exports = function() {
   const fs = require('fs-extra');
   const glob = require('glob');
 
-  const apmDependenciesPath = path.join(CONFIG.apmRootPath, 'node_modules');
-  console.log(`Cleaning ${apmDependenciesPath}`);
-  fs.removeSync(apmDependenciesPath);
+  const removePath = async p => {
+    console.log(`Cleaning ${p}`);
+    await fs.remove(p);
+    console.log(`Cleaned ${p}`);
+  }
 
+  const apmDependenciesPath = path.join(CONFIG.apmRootPath, 'node_modules');
   const atomDependenciesPath = path.join(
     CONFIG.repositoryRootPath,
     'node_modules'
   );
-  console.log(`Cleaning ${atomDependenciesPath}`);
-  fs.removeSync(atomDependenciesPath);
-
   const scriptDependenciesPath = path.join(
     CONFIG.scriptRootPath,
     'node_modules'
   );
-  console.log(`Cleaning ${scriptDependenciesPath}`);
-  fs.removeSync(scriptDependenciesPath);
 
-  const bundledPackageDependenciesPaths = path.join(
-    CONFIG.repositoryRootPath,
-    'packages',
-    '**',
-    'node_modules'
-  );
-  for (const bundledPackageDependencyPath of glob.sync(
-    bundledPackageDependenciesPaths
-  )) {
-    fs.removeSync(bundledPackageDependencyPath);
-  }
+  await Promise.all([
+    removePath(apmDependenciesPath),
+    removePath(atomDependenciesPath),
+    removePath(scriptDependenciesPath),
+    (async () => {
+      const bundledPackageDependenciesPaths = path.join(
+        CONFIG.repositoryRootPath,
+        'packages',
+        '**',
+        'node_modules'
+      );
+
+      const paths = glob.sync(
+        bundledPackageDependenciesPaths
+      );
+
+      console.log(`Cleaning ${paths}`);
+      await Promise.all(paths.map(p => fs.remove(p)));
+      console.log(`Cleaned ${paths}`);
+    })()
+  ]);
 };
